319aaa5d4decaeb74f4c483212216f6f3a9cbe61
Carla Miles <ms.carla.albert@gmail.com>
2019-07-24 11:33:35 -0500

    updated unique title validation

--- apps/tv_show_app/models.py
+++ apps/tv_show_app/models.py
@@ -5,6 +5,8 @@ from datetime import datetime
 class ShowManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
+        if Show.objects.filter(title=postData["title"]).count() > 0:
+            errors["title_unique"] = "Title must be unique"
         if len(postData['title']) < 2:
             errors["title"] = "Title name should be at least 2 characters"
         if len(postData['network']) < 3:
@@ -17,7 +19,7 @@ class ShowManager(models.Manager):
         return errors
 
 class Show(models.Model):
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
     release_date = models.DateField()
     description = models.TextField(blank=True, default='')
